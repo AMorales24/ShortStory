0edb808dcc2642bdb6dd7aa4a6619b03c09e5f51
AMorales12 <angelomorales@mail.adelphi.edu>
2018-12-15 00:20:15 -0500

    Feedback-Check

--- app/src/main/java/angelomorales/com/shortstory/MainActivity.java
+++ app/src/main/java/angelomorales/com/shortstory/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
     private Button StartButton;
 
 
-    private RecyclerView mRecyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,22 +54,22 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        mRecyclerView = (RecyclerView)findViewById(R.id.recyclerView);
+       // mRecyclerView = (RecyclerView)findViewById(R.id.recyclerView);
 
-        List<VideoObject> videos = new ArrayList<VideoObject>();
-        VideoObject story1 = new VideoObject("Story #1", "https://ia800800.us.archive.org/9/items/TheBeatlesHelpHD/The%20Beatles%20-%20Help%21%20%5BHD%5D.mp4");
-        VideoObject story2 = new VideoObject("Story #2", "https://ia601006.us.archive.org/33/items/IWantToHoldYourHandHdByTheBeatles/i%20want%20to%20hold%20your%20hand%20hd%20by%20the%20beatles.mp4");
-        VideoObject story3 = new VideoObject("Story #3", "https://ia902804.us.archive.org/4/items/TheBeatles-GetBack/TheBeatles-GetBack.mp4");
-        VideoObject story4 = new VideoObject("Story #4", "https://ia802800.us.archive.org/9/items/TheBeatles-Rain_MusicVideo/DVD_210344_0_Segment_0_x264.mp4");
+      //  List<VideoObject> videos = new ArrayList<VideoObject>();
+      //  VideoObject story1 = new VideoObject("Story #1", "https://ia800800.us.archive.org/9/items/TheBeatlesHelpHD/The%20Beatles%20-%20Help%21%20%5BHD%5D.mp4");
+      //  VideoObject story2 = new VideoObject("Story #2", "https://ia601006.us.archive.org/33/items/IWantToHoldYourHandHdByTheBeatles/i%20want%20to%20hold%20your%20hand%20hd%20by%20the%20beatles.mp4");
+      //  VideoObject story3 = new VideoObject("Story #3", "https://ia902804.us.archive.org/4/items/TheBeatles-GetBack/TheBeatles-GetBack.mp4");
+      //  VideoObject story4 = new VideoObject("Story #4", "https://ia802800.us.archive.org/9/items/TheBeatles-Rain_MusicVideo/DVD_210344_0_Segment_0_x264.mp4");
 
-        videos.add(story1);
-        videos.add(story2);
-        videos.add(story3);
-        videos.add(story4);
+       // videos.add(story1);
+     //   videos.add(story2);
+      //  videos.add(story3);
+      //  videos.add(story4);
 
-        VideoAdapter adapter = new VideoAdapter(videos);
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
-        mRecyclerView.setAdapter(adapter);
+       // VideoAdapter adapter = new VideoAdapter(videos);
+       // mRecyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+     //   mRecyclerView.setAdapter(adapter);
 
 
 
